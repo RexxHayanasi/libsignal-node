875044d0e0bf774db0f3283a4bdff969b61203b9
RexxHayanasi mau S.Kom <rexxhayanasi@gmail.com>
2026-01-05 19:11:30 +0700

    Update crypto.d.ts

--- src/crypto.d.ts
+++ src/crypto.d.ts
@@ -1,22 +1,19 @@
-export interface KeyPairType {
-  pubKey: Uint8Array;
-  privKey: Uint8Array;
-}
+export function decrypt(
+  key: Uint8Array,
+  ciphertext: Uint8Array,
+  iv: Uint8Array
+): Promise<Uint8Array>;
 
-export function generateKeyPair(): KeyPairType;
+export function encrypt(
+  key: Uint8Array,
+  plaintext: Uint8Array,
+  iv: Uint8Array
+): Promise<Buffer>;
 
-export function calculateAgreement(
-  publicKey: Uint8Array,
-  privateKey: Uint8Array
-): Uint8Array;
+export function calculateMAC(key: Buffer, data: Uint8Array): Uint8Array;
 
-export function calculateSignature(
-  privateKey: Uint8Array,
-  message: Uint8Array
-): Uint8Array;
-
-export function verifySignature(
-  publicKey: Uint8Array,
-  message: Uint8Array,
-  signature: Uint8Array
-): boolean;
+export function deriveSecrets(
+  key: Uint8Array,
+  salt: Buffer,
+  info: Buffer
+): [Buffer, Buffer];
